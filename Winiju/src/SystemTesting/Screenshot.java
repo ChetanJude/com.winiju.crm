ea456175d8237c48ce0de4b125035bcb1d592652
Chetan <Chetan@Chetan-pc>
2024-08-30 18:07:59 +0530

    Winiju

---
+++ Winiju/src/SystemTesting/Screenshot.java
@@ -0,0 +1,24 @@
+package SystemTesting;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Screenshot {
+
+	public static void main(String[] args) throws IOException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://chatgpt.com/");
+		TakesScreenshot t=(TakesScreenshot) driver;
+		File src = t.getScreenshotAs(OutputType.FILE);
+		File dest= new File("./screenshot/ss.png");
+		FileUtils.copyFile(src, dest);
+		driver.quit();
+	}
+
+}
---
+++ Winiju/src/SystemTesting/Winiju_Camp_Lead_ST.java
@@ -0,0 +1,37 @@
+package SystemTesting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+public class Winiju_Camp_Lead_ST {
+
+	public static void main(String[] args) {
+		/*
+		 * WebDriver driver = new ChromeDriver();
+		 * driver.get("http://192.168.84.128:8080/crm/HomePage.do"); String
+		 * expectlogintitle="Zoho CRM - Sign in"; String
+		 * actuallogintitle=driver.getTitle(); Assert.assertEquals(actuallogintitle,
+		 * expectlogintitle);
+		 * driver.findElement(By.id("userName")).sendKeys("rashmi@dell.com");
+		 * driver.findElement(By.id("passWord")).sendKeys("123456");
+		 * driver.findElement(By.xpath("//input[@title='Sign In']")).click(); String
+		 * expecthometitle="Zoho CRM - Home Page"; String
+		 * actualhometitle=driver.getTitle(); Assert.assertEquals(actualhometitle,
+		 * expecthometitle);
+		 * driver.findElement(By.xpath("//a[text()='Campaigns']")).click();
+		 * driver.findElement(By.xpath("//input[@value='New Campaign']")).click();
+		 * WebElement alllisttype = driver.findElement(By.name("property(Type)"));
+		 * Select s=new Select(alllisttype); s.selectByValue("Webinar"); WebElement
+		 * allliststatus = driver.findElement(By.name("property(Status)")); Select
+		 * s1=new Select(allliststatus); s1.selectByValue("Planning");
+		 * driver.findElement(By.xpath("(//img[@title='Calendar'])[2]")).click();
+		 * driver.getWindowHandles(). //driver.switchTo().window(actualhometitle);
+		 * //driver.findElement(By.xpath("//a[text()='22']")).click();
+		 */
+	}
+
+}
